a47ff82404f2b7091be41e8b8dfa093f4b61827c
Ryan <ryan.e.levi@gmail.com>
2013-08-10 19:41:32 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lyricscloud::Application.config.secret_token = 'd2f19fcd3d92ce6a47fa2fa89b2f31b9d40999adae3b57e98d11b07366c3a94be1522e5ef83cf56997b024c23d6fb200c620c8a8548c4dba6f41eabd176b55c5'
