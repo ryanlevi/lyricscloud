24c48db2f0666d50ad3e60e57175120f1f8bccbe
Ryan <ryan.e.levi@gmail.com>
2013-10-21 11:25:41 -0700

    Listing songs under lyricscloud

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -14,9 +14,10 @@ class HomeController < ApplicationController
   def create_album
     artist = params[:artist].to_s
     title = params[:title].to_s
-    song = GetAlbum.new(artist, title)
-    song.words_list = song.remove_small_words
-    @array = song.get_lyrics(100)
+    album = GetAlbum.new(artist, title)
+    album.words_list = album.remove_small_words
+    @album_tracks = album.album_tracks
+    @array = album.get_lyrics(100)
     render "index"
   end
 
